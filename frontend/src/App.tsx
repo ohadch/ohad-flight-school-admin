71d8d3933d7cbdc00ac420d3a6351a4bc2a4cb8b
ohadchaet <ohad.chaet@lynx.md>
2023-05-18 21:15:37 +0300

    feat: nice app container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,161 @@
-import React, {useState} from "react";
-import {AppBar, css, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography} from "@mui/material";
+import * as React from 'react';
+import {styled, createTheme, ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PersonIcon from '@mui/icons-material/Person';
+import List from '@mui/material/List';
+import {ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 
-const App = () => {
-  const [openDrawer, setOpenDrawer] = useState(false);
+const DRAWER_WIDTH: number = 240;
 
-  const styles = css`
-    .root {
-      width: 100vw;
-      height: 100vh;
-    }
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
 
-    .appBar {
-      backgroundColor: primary.main;
-    }
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({theme, open}) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: DRAWER_WIDTH,
+        width: `calc(100% - ${DRAWER_WIDTH}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
 
-    .drawer {
-      width: 250px;
-    }
+const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
+    ({theme, open}) => ({
+        '& .MuiDrawer-paper': {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: DRAWER_WIDTH,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            boxSizing: 'border-box',
+            ...(!open && {
+                overflowX: 'hidden',
+                transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                }),
+                width: theme.spacing(7),
+                [theme.breakpoints.up('sm')]: {
+                    width: theme.spacing(9),
+                },
+            }),
+        },
+    }),
+);
 
-    .listItem {
-      height: 40px;
-    }
+const defaultTheme = createTheme();
 
-    .listItemText {
-      fontSize: 16px;
-    }
-  `;
+export default function App() {
+    const [open, setOpen] = React.useState(true);
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
 
-  return (
-    <div css={styles}>
-      <AppBar position="static" color="primary">
-        <Typography variant="h4" color="white">My App</Typography>
-        <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
-          <i className="material-icons">menu</i>
-        </IconButton>
-      </AppBar>
-      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
-        <List>
-          <ListItem button key="1">
-            <ListItemIcon>
-              <i className="material-icons">settings</i>
-            </ListItemIcon>
-            <ListItemText primary="Settings"></ListItemText>
-          </ListItem>
-          <ListItem button key="2">
-            <ListItemIcon>
-              <i className="material-icons">help</i>
-            </ListItemIcon>
-            <ListItemText primary="Help"></ListItemText>
-          </ListItem>
-        </List>
-      </Drawer>
-      <main>
-        <h1>Main content</h1>
-      </main>
-    </div>
-  );
-};
-
-export default App;
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Box sx={{display: 'flex'}}>
+                <CssBaseline/>
+                <AppBar position="absolute" open={open}>
+                    <Toolbar
+                        sx={{
+                            pr: '24px', // keep right padding when drawer closed
+                        }}
+                    >
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={toggleDrawer}
+                            sx={{
+                                marginRight: '36px',
+                                ...(open && {display: 'none'}),
+                            }}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography
+                            component="h1"
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                            sx={{flexGrow: 1}}
+                        >
+                            Dashboard
+                        </Typography>
+                        <IconButton color="inherit">
+                            <Badge badgeContent={4} color="secondary">
+                                <NotificationsIcon/>
+                            </Badge>
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+                <Drawer variant="permanent" open={open}>
+                    <Toolbar
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'flex-end',
+                            px: [1],
+                        }}
+                    >
+                        <IconButton onClick={toggleDrawer}>
+                            <ChevronLeftIcon/>
+                        </IconButton>
+                    </Toolbar>
+                    <Divider/>
+                    <List component="nav">
+                        <React.Fragment>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <PersonIcon/>
+                                </ListItemIcon>
+                                <ListItemText primary="Members"/>
+                            </ListItemButton>
+                        </React.Fragment>
+                    </List>
+                </Drawer>
+                <Box
+                    component="main"
+                    sx={{
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === 'light'
+                                ? theme.palette.grey[100]
+                                : theme.palette.grey[900],
+                        flexGrow: 1,
+                        height: '100vh',
+                        overflow: 'auto',
+                    }}
+                >
+                    <Toolbar/>
+                    <Container maxWidth="lg" sx={{mt: 4, mb: 4}}>
+                        Content goes here
+                    </Container>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
+}
