dcdfcac8a086442e518a16ba114ac42c8804b388
ohadchaet <ohad.chaet@lynx.md>
2023-05-18 10:37:57 +0300

    feat: adds react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,61 @@
-import './App.css'
+import React, {useState} from "react";
+import {AppBar, css, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography} from "@mui/material";
 
-function App() {
-    return (
-        <>
-            <div>
-                <h1>App</h1>
-            </div>
-        </>
-    )
-}
+const App = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
 
-export default App
+  const styles = css`
+    .root {
+      width: 100vw;
+      height: 100vh;
+    }
+
+    .appBar {
+      backgroundColor: primary.main;
+    }
+
+    .drawer {
+      width: 250px;
+    }
+
+    .listItem {
+      height: 40px;
+    }
+
+    .listItemText {
+      fontSize: 16px;
+    }
+  `;
+
+  return (
+    <div css={styles}>
+      <AppBar position="static" color="primary">
+        <Typography variant="h4" color="white">My App</Typography>
+        <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+          <i className="material-icons">menu</i>
+        </IconButton>
+      </AppBar>
+      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <List>
+          <ListItem button key="1">
+            <ListItemIcon>
+              <i className="material-icons">settings</i>
+            </ListItemIcon>
+            <ListItemText primary="Settings"></ListItemText>
+          </ListItem>
+          <ListItem button key="2">
+            <ListItemIcon>
+              <i className="material-icons">help</i>
+            </ListItemIcon>
+            <ListItemText primary="Help"></ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+      <main>
+        <h1>Main content</h1>
+      </main>
+    </div>
+  );
+};
+
+export default App;
