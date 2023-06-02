7ee02816f90d8d3d02a0d5d759790aebc7077cd0
ohadchaet <ohad.chaet@lynx.md>
2023-06-02 11:47:17 +0300

    fix: missing properties in types

--- frontend/src/@types/models/MemberDocument/types.ts
+++ frontend/src/@types/models/MemberDocument/types.ts
@@ -1,18 +1,21 @@
-import {MemberDocumentType} from "./enums.ts";
+import {MemberDocumentStatus, MemberDocumentType} from "./enums.ts";
 
 export interface IMemberDocument {
     id: number;
     member_id: number;
     type: MemberDocumentType;
+    status: MemberDocumentStatus;
     expiration_at: string | null;
 }
 
 export interface IMemberDocumentCreate {
     type: MemberDocumentType;
+    status: MemberDocumentStatus;
     expiration_at: string | null;
 }
 
 export interface IMemberDocumentUpdate {
     type: MemberDocumentType;
+    status: MemberDocumentStatus;
     expiration_at: string | null;
 }
